ad0e4a941062514d9722330362dff77246abb415
Sonny Li <sonnynomnom@gmail.com>
2025-04-07 01:22:59 -0400

    Update and rename 07_my_data.py to 07_linear_search.py

---
+++ 2-list-algorithms/07_linear_search.py
@@ -0,0 +1,33 @@
+# Linear Search 📧
+# Codédex
+
+def linear_search(input_list, target_value):
+  for item in input_list:
+    if item == target_value:
+      return True
+  return False
+
+email_list = [
+  'dwight.schrute@dundermifflin.com', 
+  'michael.scott@dundermifflin.com',
+  'mochi@codedex.io',
+  'mgoodyear@lumonindustries.com',
+  'walter.white@jpwynnehigh.edu',
+  'hank@dea.gov',
+  'kimberly.finkle@essexu.edu',
+  'sheldon@caltech.edu',
+  'elliot@allsafe.com',
+  'mr.robot@fsociety.com',
+  'mulder@fbi.gov',
+  'carrie@sexandthecity.tvs',
+  'pleasecallmebarney@yahoo.com',
+  'buffy@sunnydale.edu'
+]
+
+print(linear_search(email_list, 'mgoodyear@lumonindustries.com'))   # Output: True
+print(linear_search(email_list, 'mark.scout@lumonindustries.com'))  # Output: False
+
+# Checking an edge case - what if our input list is empty?
+
+empty_list = []
+print(linear_search(empty_list, 'dwight@dundermifflin.com'))        # Output: False
