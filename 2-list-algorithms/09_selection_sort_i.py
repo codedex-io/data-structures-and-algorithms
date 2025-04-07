b64254c4f1e74af6563d228f07ee39b455f1a47f
Sonny Li <sonnynomnom@gmail.com>
2025-04-07 03:37:17 -0400

    Update and rename 09_efficiency.py to 09_selection_sort_i.py

---
+++ 2-list-algorithms/09_selection_sort_i.py
@@ -0,0 +1,28 @@
+# Selection Sort 📶
+# Codédex
+
+my_list = [8, 15, 4, 2]
+
+# Step 1: Write Swap and test it!
+
+def swap(input_list, index_1, index_2):
+  temp = input_list[index_1] # Using temp so we don't lose the original value at index_1
+  input_list[index_1] = input_list[index_2]
+  input_list[index_2] = temp
+  return input_list # Make sure to return the swapped list
+
+
+# The lowest index before we begin the for loop is 0
+
+lowest_index = 0
+
+# Step 2: Write the for loop that finds the lowest index in the list
+
+for i in range(len(my_list)):
+  if my_list[i] < my_list[lowest_index]:
+    lowest_index = i
+
+# Step 3: After finding the lowest index, use swap() to put the lowest value at the start of the list
+
+my_list = swap(my_list, lowest_index, 0)
+print(my_list)
